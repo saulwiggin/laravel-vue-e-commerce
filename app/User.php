198eee1215781221cd2c1169865a5fc93862fbbf
saul.wiggin <saul.wiggin@retrospect.com>
2020-08-31 16:43:53 +0100

    add verification

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use App\Notifications\ResetPassword;
 use App\Notifications\VerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject //, MustVerifyEmail
